e48041165f9602c09f29430998daeb9e5e6c5058
Will Canniford <willcanniford@yahoo.co.uk>
2020-02-08 15:49:00 +0000

    Replace code with call to connection helper function

--- classes/Fixture.py
+++ classes/Fixture.py
@@ -1,18 +1,10 @@
-# Imports 
 import os
 import pymongo
+from .helpers import *
 
 
 class Fixture():
     def __init__(self, fixture_id):
         self.fixture_id = fixture_id
-        mongodb_password = os.environ.get("mongodb_password")
-        mongodb_user = os.environ.get("mongodb_user")
-        client = pymongo.MongoClient(
-            "mongodb+srv://" +
-            mongodb_user +
-            ":" +
-            mongodb_password +
-            "@basiccluster-6s0er.mongodb.net/test?retryWrites=true&w=majority")
-        db = client.football
+        db = connect_to_mongodb('football')
         self.response = db.fixtures.find_one({'fixture_id': fixture_id})
